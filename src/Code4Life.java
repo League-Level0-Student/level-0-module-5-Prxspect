900b38c30808636f85290e0981647d3ac88536c6
B92064 <league@wts5.attlocal.net>
2018-04-14 12:54:38 -0700

    CircleRing,Code4Life,EvilPolygonsChallenge

--- src/Code4Life.java
+++ src/Code4Life.java
@@ -7,20 +7,33 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class Code4Life {
 	/*
      * Ask the user how many hours they spent coding this week.
-     * 
+     
+     
      * 1. If it's 3 or more, tell them they're a Code Ninja.
      * 
      * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
      * 
      * 3. If it's more than 5, play the Batman theme song.
      */
-
-
+public static void main(String[] args) {
+	String codehrs=JOptionPane.showInputDialog("How many hours did you spend coding this week?");
+	int hrs=Integer.parseInt(codehrs);
+	if(hrs>=3) {
+		JOptionPane.showMessageDialog(null, "You are a Code Ninja!");
+	}
+	else if(hrs<2) {
+		JOptionPane.showMessageDialog(null, "Stop watching Youtube and write some code!");
+	}
+	else if(hrs>5) {
+		playBatmanTheme();
+	}
+}
     private static void playBatmanTheme() {
     	try {
    		 Clip clip = AudioSystem.getClip();
--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -8,17 +8,53 @@ public class EvilPolygonsChallenge {
 	
 	public static void main(String[] args) {
 		//1. Create a robot
-
+Robot r2d2=new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+r2d2.setX(50);
+r2d2.setY(300);
+r2d2.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "Hello! What color do you want your polygons?", "Color Chooser", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Purple","Green","Blue","Yellow","Random Color"}, 0);
 		
 		//3. Set the pen color of the robot according to the value of the colorChoise variable
-		
+		if(colorChoice==0) {
+			r2d2.setPenColor(252,13,13);
+		}
+		else if(colorChoice==1) {
+			r2d2.setPenColor(200,13,252);
+		}
+		else if(colorChoice==2) {
+			r2d2.setPenColor(63,252,13);
+		}
+		else if(colorChoice==3) {
+			r2d2.setPenColor(15,24,242);
+		}
+		else if(colorChoice==4) {
+			r2d2.setPenColor(248,252,3);
+		}
+		else if(colorChoice==5) {
+			r2d2.setRandomPenColor();
+		}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String polygons=JOptionPane.showInputDialog("How many polygons do you want to be drawn?");
+		int num=Integer.parseInt(polygons);
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		for (int i = 0; i < num; i++) {
+			r2d2.penDown();
+			r2d2.move(50);
+			r2d2.turn(45);
+			r2d2.move(50);
+			r2d2.turn(90);
+			r2d2.move(50);
+			r2d2.turn(45);
+			r2d2.move(50);
+			r2d2.turn(90);
+			r2d2.move(70);
+			r2d2.penUp();
+			r2d2.turn(180);
+			r2d2.move(100);
+			r2d2.turn(-90);
+		}
+		r2d2.sparkle();
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
